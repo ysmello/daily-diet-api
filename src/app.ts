5fd236d6a10aa2ddf3cbd68deede2c8a51664676
Yuri Mello <yuridemeello@gmail.com>
2023-06-02 07:11:22 -0300

    add fatify cookie

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import fastify from 'fastify'
+import fastifyCookie from '@fastify/cookie'
+
 import { usersRoutes } from './routes/users'
 import { dietsRoutes } from './routes/diets'
 
 export const app = fastify()
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes, {
   prefix: '/users',
