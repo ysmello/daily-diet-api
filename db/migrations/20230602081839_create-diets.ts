5e6c75266f62d2874f80b5aaf532507700ce4c8f
Yuri Mello <yuridemeello@gmail.com>
2023-06-02 07:11:05 -0300

    fix diets migration

--- db/migrations/20230602081839_create-diets.ts
+++ db/migrations/20230602081839_create-diets.ts
@@ -5,7 +5,7 @@ export async function up(knex: Knex): Promise<void> {
     table.uuid('id').primary()
     table.string('name').notNullable()
     table.text('description').notNullable()
-    table.timestamp('created_at').defaultTo(knex.fn.now).notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
   })
 }
 
